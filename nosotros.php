a1c16073411a4e651159249adf4a332fde415e9e
Alejandro Kaleb Leyte Garcia <78818308+KalLeyte@users.noreply.github.com>
2021-09-30 15:48:40 -0500

    Add files via upload

---
+++ nosotros.php
@@ -0,0 +1,217 @@
+<!-- inicio nombre -->
+<?php $nombrePagina = 'Nosotros'; ?>
+<!-- fin nombre -->
+
+<!-- inicio header -->
+<?php
+include 'includes/header.php';
+?>
+<!-- fin header -->
+
+<!-- inicio barra de navegacion -->
+<?php
+include 'includes/navegacion.php';
+?>
+<!-- fin barra de navegacion -->
+
+<!-- banner -->
+<div class="bgimagen-inicio">
+  <div class="container animated wow zoomIn mt-5">
+    <div class="titulo-bgimagen my-5">
+      <h2>Nuestra biblioteca</h2>
+    </div>
+  </div>
+</div>
+<!-- fin banner -->
+
+<!-- grid 1 -->
+<div class="container">
+  <h2 class="Nosotros">encargados</h2>
+  <center>
+    <div class="card mb-6 card text-white bg-dark mb-3 table-hover table-dark border border-primary aso1" style="max-width: 500px;">
+      <div class="row no-gutters">
+        <div class="col-md-7 ">
+          <div class="card-body">
+            <h5 class="card-title titul">Victor manuel vidal zavala</h5>
+            <p class="card-text"> Encargado de la biblioteca </p>
+          </div>
+        </div>
+        <div class="col-md-5 border border-primary">
+          <img src="img/profe.jpg" class="card-img border-primary" alt="...">
+        </div>
+      </div>
+    </div>
+  </center>
+</div>
+<!-- fin grid 1 -->
+
+<!-- grid 2 -->
+<div class="container">
+  <h2 class="Nosotros">areas de biblioteca</h2>
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">acervo bibliografico</h3>
+          <p class="card-text">Cuenta con libros para distintas materias dividios por area las cuales son:</p>
+          <li>Generales: Libros desde literatura hasta diccionarios</li>
+          <li>Basicas: Materias que en todos los grados se usan</li>
+          <li>Humanisticas: Referentes al cuerpo humano</li>
+          <li>Tecnologicas: hace alucion a las carreras de la escuela y a la tecnologia </li>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">hemeroteca</h3>
+          <p class="card-text">Cuenta con distintos articulos en forma de:
+          <p>
+            <li>Revistas: Desde temas de ciencia hasta temas relacionados con las materias</li>
+            <li>Tripticos: Realizados por estudiantes u obtenidos de otras areas </li>
+          <p class="card-text">Todas estas adquisiciones son sobre temas educativos e informativos
+          <p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">videoteca</h3>
+          <p class="card-text">Tambien contamos con informacion almacenada en formate de:</p>
+          <li>CD ROM: Discos compactos con informacion almacenada</li>
+          <li>DVD: peliculas en este formato</li>
+          <li>CD AUDIO: Audios sobre temas educativos</li>
+          <p class="card-text">Este contenido es solo educativo
+          <p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">servicio de computo</h3>
+          <li>Tenemos un area de computo con acceso a internet, con un total de 6 laptops HP, listas para usarse; ademas de que cuentan con acceso directo a paginas de interes que les pueden servir para ampliar su conocimiento</li>
+          <li>El uso indevido de este servicio (busqueda de sitios para adultos, instalaciones no autorizadas, etc.) sera acredor a una sancion</li>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- fin grid 2 -->
+
+<!-- grid 3 -->
+</div>
+<div class="container">
+  <h2 class="Nosotros">informacion</h2>
+  <div class="row">
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">Identificación</h3>
+          <p class="card-text">Los préstamos se realizarán únicamente con credencial de la biblioteca, la cual se puede obtener en la biblioteca, con el encargado, donde se llenara un formato, ademas de dejar su credencial escolar para realizar el tramite y de esta forma poder tomar prestado alguno o algunos libros del acervo bibliografico, hemeroteca y videoteca. </p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">credenciales extraviadas</h3>
+          <p class="card-text">Nadie podrá solicitar préstamo con credencial ajena, quien pretenda hacerlo perderá todo derecho a usar la biblioteca, pues las credenciales son personales e intransferibles. El centro de información no se hace responsable de las credenciales extraviadas, ni del mal uso que se haga de ellas. Dicha responsabilidad recae en el propietario. </p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">registro de préstamos</h3>
+          <p class="card-text">Todo material de la biblioteca debe solicitarse, registrarse y devolverse en el lugar correspondiente. Quien intente llevarse un material, sea alumno, profesor, trabajador o directivo sin registrarlo debidamente, perderá todo derecho a servicios bibliotecarios. Se le aplicará el reglamento de préstamo y además será turnado a la subdirección académica para que se haga lo pertinente.</p>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">libros perdidos</h3>
+          <p class="card-text">La pérdida de un libro debe reportarse inmediatamente. El usuario tiene que remplazar el material extraviado, debidamente encuadernado y además entregar otro libro, de valor equivalente al que perdió, de lo cual recibirá un comprobante en calidad de donación.</p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">objetos maltratados</h3>
+          <p class="card-text">Todo usuario que maltrate un libro, revista o enciclopedia, subrayando palabras, borrando o recortando,etc,deberá reponer íntegramente el material y además tendrá que entregar otro libro con valor equivalente al daño y recibirá un comprobante en calidad de donación.</p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">revisión de libros prestados</h3>
+          <p class="card-text">Quien solicita un libro está obligado a revisar que se encuentre en perfecto estado, y de no ser así deberá reportarlo. El personal bibliotecario tendrá especial cuidado cuando recoja un libro verificado que este en las mismas condiciones en que fue prestado.</p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">duración del préstamo y sanciones</h3>
+          <p class="card-text">El establecimiento de tiempos en la duración de los préstamos y las sanciones, se aplican con el objetivo de que un mayor número de usuarios sea beneficiario de los servicios que se proporcionan, para que ninguna persona tenga problema en usar alguna adquisicion que tiene la biblioteca. </p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">préstamo a trabajadores del plantel</h3>
+          <p class="card-text">Sin excepción todos los trabajadores (académicos,administrativos y directivos) del plantel se sujetarán al presente reglamento y tendrán que presentar su credencial institucional actualizada. En caso de no hacerlo se recurrirán a las instancias ya señaladas en el reglamento. </p>
+
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 ">
+      <div class="card mb-4 text-white  table-hover table-dark border border-primary aso1">
+        <div class="card-body">
+          <h3 class="titul">préstamo a personas ajenas al plantel</h3>
+          <p class="card-text">El acervo de la biblioteca esta fundamentalmente orientado a satisfacer las necesidades pedagogícas de estudiantes a nivel medio superior por lo cual estudiantes de otras dependencias, el servicio se le proporcionará mediante el préstamo interbibliotecario.</p>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<!-- fin grid 3 -->
+
+<!-- inicio footer -->
+<?php
+include 'includes/footer.php';
+?>
+<!-- fin footer -->
+
+<!-- archivos js -->
+<?php
+include 'includes/scripts.php';
+?>
+<!-- fin archivos js -->
+</body>
+
+</html>
